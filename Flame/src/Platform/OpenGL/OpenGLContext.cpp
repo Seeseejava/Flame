c97b7efd2b01369e9f88d466e3e3ec83beca5409
Seeseejava <49669869+Seeseejava@users.noreply.github.com>
2022-10-08 11:51:25 +0800

    补充检查OpenGL版本

--- Flame/src/Platform/OpenGL/OpenGLContext.cpp
+++ Flame/src/Platform/OpenGL/OpenGLContext.cpp
@@ -24,6 +24,15 @@ namespace Flame {
 		FLAME_CORE_INFO("Renderer: {0}", glGetString(GL_RENDERER));
 		FLAME_CORE_INFO("Version: {0}", glGetString(GL_VERSION));
 
+		// ¼ì²éOpenGL°æ±¾
+		#ifdef FLAME_ENABLE_ASSERTS
+				int versionMajor;
+				int versionMinor;
+				glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+				glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+				FLAME_CORE_ASSERT((versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5)), "Hazel requires at least OpenGL version 4.5!");
+		#endif
 
 	}
 	void OpenGLContext::SwapBuffers()
