475403fb7c42521ca3f368daee34f6908e9fd87d
Seeseejava <49669869+Seeseejava@users.noreply.github.com>
2022-10-17 11:12:24 +0800

    Renderer2D: added draw call splitting and stats
    --已经可以实现draw call batch功能了
    --实现statistic结构体用来记录drawcall并进行split

--- Flame/src/Flame/Renderer/Renderer2D.cpp
+++ Flame/src/Flame/Renderer/Renderer2D.cpp
@@ -22,9 +22,9 @@ namespace Flame {
 
 	struct Renderer2DData
 	{
-		const uint32_t MaxQuads = 10000;
-		const uint32_t MaxVertices = MaxQuads * 4;
-		const uint32_t MaxIndices = MaxQuads * 6;
+		static const uint32_t MaxQuads = 10000;
+		static const uint32_t MaxVertices = MaxQuads * 4;
+		static const uint32_t MaxIndices = MaxQuads * 6;
 		static const uint32_t MaxTextureSlots = 32; // TODO: RenderCaps
 
 		Ref<VertexArray> QuadVertexArray;
@@ -40,6 +40,8 @@ namespace Flame {
 		uint32_t TextureSlotIndex = 1; // 0 = white texture
 
 		glm::vec4 QuadVertexPositions[4];
+
+		Renderer2D::Statistics Stats;
 	};
 
 	// 这里设置为指针是为了内存管理，设置为指针后要记得初始化
@@ -141,6 +143,18 @@ namespace Flame {
 			s_Data->TextureSlots[i]->Bind(i);
 
 		RenderCommand::DrawIndexed(s_Data->QuadVertexArray, s_Data->QuadIndexCount);
+		s_Data->Stats.DrawCalls++;
+	}
+
+	void Renderer2D::FlushAndReset()
+	{
+		EndScene();
+
+		//相较于BeginScene()区别：不用再绑定vp矩阵和摄像机
+		s_Data->QuadIndexCount = 0;
+		s_Data->QuadVertexBufferPtr = s_Data->QuadVertexBufferBase;
+
+		s_Data->TextureSlotIndex = 1;
 	}
 
 	void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color)
@@ -151,6 +165,9 @@ namespace Flame {
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color)
 	{
 		FLAME_PROFILE_FUNCTION();
+
+		if (s_Data->QuadIndexCount >= Renderer2DData::MaxIndices)
+			FlushAndReset();
 
 		const float textureIndex = 0.0f; // White Texture
 		const float tilingFactor = 1.0f;
@@ -187,6 +204,8 @@ namespace Flame {
 		s_Data->QuadVertexBufferPtr++;
 			  
 		s_Data->QuadIndexCount += 6;
+
+		s_Data->Stats.QuadCount++;
 	}
 
 	void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
@@ -197,6 +216,9 @@ namespace Flame {
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
 	{
 		FLAME_PROFILE_FUNCTION();
+
+		if (s_Data->QuadIndexCount >= Renderer2DData::MaxIndices)
+			FlushAndReset();
 
 		constexpr glm::vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
 
@@ -249,6 +271,8 @@ namespace Flame {
 		s_Data->QuadVertexBufferPtr++;
 
 		s_Data->QuadIndexCount += 6;
+
+		s_Data->Stats.QuadCount++;
 	}
 
 	void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const glm::vec4& color)
@@ -259,6 +283,9 @@ namespace Flame {
 	void Renderer2D::DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const glm::vec4& color)
 	{
 		FLAME_PROFILE_FUNCTION();
+
+		if (s_Data->QuadIndexCount >= Renderer2DData::MaxIndices)
+			FlushAndReset();
 
 		const float textureIndex = 0.0f; // White Texture
 		const float tilingFactor = 1.0f;
@@ -296,6 +323,8 @@ namespace Flame {
 		s_Data->QuadVertexBufferPtr++;
 
 		s_Data->QuadIndexCount += 6;
+
+		s_Data->Stats.QuadCount++;
 	}
 
 	void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
@@ -306,6 +335,9 @@ namespace Flame {
 	void Renderer2D::DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
 	{
 		FLAME_PROFILE_FUNCTION();
+
+		if (s_Data->QuadIndexCount >= Renderer2DData::MaxIndices)
+			FlushAndReset();
 
 		constexpr glm::vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
 
@@ -359,5 +391,17 @@ namespace Flame {
 		s_Data->QuadVertexBufferPtr++;
 
 		s_Data->QuadIndexCount += 6;
+
+		s_Data->Stats.QuadCount++;
+	}
+
+	void Renderer2D::ResetStats()
+	{
+		memset(&s_Data->Stats, 0, sizeof(Statistics));
+	}
+
+	Renderer2D::Statistics Renderer2D::GetStats()
+	{
+		return s_Data->Stats;
 	}
 }
