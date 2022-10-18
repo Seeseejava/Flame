bf9bac5859d45b28ea1973f076125fdbd3a76812
Seeseejava <49669869+Seeseejava@users.noreply.github.com>
2022-10-18 09:37:26 +0800

    Replace Renderer2DData::QuadVertexBuffer setup code with a for loop.

--- Flame/src/Flame/Renderer/Renderer2D.cpp
+++ Flame/src/Flame/Renderer/Renderer2D.cpp
@@ -166,42 +166,27 @@ namespace Flame {
 	{
 		FLAME_PROFILE_FUNCTION();
 
+		constexpr size_t quadVertexCount = 4;
+		const float textureIndex = 0.0f; // White Texture
+		constexpr glm::vec2 textureCoords[] = { { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 1.0f, 1.0f }, { 0.0f, 1.0f } };
+		const float tilingFactor = 1.0f;
+
 		if (s_Data->QuadIndexCount >= Renderer2DData::MaxIndices)
 			FlushAndReset();
 
-		const float textureIndex = 0.0f; // White Texture
-		const float tilingFactor = 1.0f;
 
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
 			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
 
-		s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[0];
-		s_Data->QuadVertexBufferPtr->Color = color;
-		s_Data->QuadVertexBufferPtr->TexCoord = { 0.0f, 0.0f };
-		s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
-		s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
-		s_Data->QuadVertexBufferPtr++;
-			
-		s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[1];
-		s_Data->QuadVertexBufferPtr->Color = color;
-		s_Data->QuadVertexBufferPtr->TexCoord = { 1.0f, 0.0f };
-		s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
-		s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
-		s_Data->QuadVertexBufferPtr++;
-			
-		s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[2];
-		s_Data->QuadVertexBufferPtr->Color = color;
-		s_Data->QuadVertexBufferPtr->TexCoord = { 1.0f, 1.0f };
-		s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
-		s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
-		s_Data->QuadVertexBufferPtr++;
-	
-		s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[3];
-		s_Data->QuadVertexBufferPtr->Color = color;
-		s_Data->QuadVertexBufferPtr->TexCoord = { 0.0f, 1.0f };
-		s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
-		s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
-		s_Data->QuadVertexBufferPtr++;
+		for (size_t i = 0; i < quadVertexCount; i++)
+		{
+			s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[i];
+			s_Data->QuadVertexBufferPtr->Color = color;
+			s_Data->QuadVertexBufferPtr->TexCoord = textureCoords[i];
+			s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
+			s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
+			s_Data->QuadVertexBufferPtr++;
+		}
 			  
 		s_Data->QuadIndexCount += 6;
 
@@ -220,7 +205,9 @@ namespace Flame {
 		if (s_Data->QuadIndexCount >= Renderer2DData::MaxIndices)
 			FlushAndReset();
 
+		constexpr size_t quadVertexCount = 4;
 		constexpr glm::vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
+		constexpr glm::vec2 textureCoords[] = { { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 1.0f, 1.0f }, { 0.0f, 1.0f } };
 
 		float textureIndex = 0.0f;
 		for (uint32_t i = 1; i < s_Data->TextureSlotIndex; i++)
@@ -242,33 +229,15 @@ namespace Flame {
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
 			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
 
-		s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[0];
-		s_Data->QuadVertexBufferPtr->Color = color;
-		s_Data->QuadVertexBufferPtr->TexCoord = { 0.0f, 0.0f };
-		s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
-		s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
-		s_Data->QuadVertexBufferPtr++;
-
-		s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[1];
-		s_Data->QuadVertexBufferPtr->Color = color;
-		s_Data->QuadVertexBufferPtr->TexCoord = { 1.0f, 0.0f };
-		s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
-		s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
-		s_Data->QuadVertexBufferPtr++;
-
-		s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[2];
-		s_Data->QuadVertexBufferPtr->Color = color;
-		s_Data->QuadVertexBufferPtr->TexCoord = { 1.0f, 1.0f };
-		s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
-		s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
-		s_Data->QuadVertexBufferPtr++;
-
-		s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[3];
-		s_Data->QuadVertexBufferPtr->Color = color;
-		s_Data->QuadVertexBufferPtr->TexCoord = { 0.0f, 1.0f };
-		s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
-		s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
-		s_Data->QuadVertexBufferPtr++;
+		for (size_t i = 0; i < quadVertexCount; i++)
+		{
+			s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[i];
+			s_Data->QuadVertexBufferPtr->Color = color;
+			s_Data->QuadVertexBufferPtr->TexCoord = textureCoords[i];
+			s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
+			s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
+			s_Data->QuadVertexBufferPtr++;
+		}
 
 		s_Data->QuadIndexCount += 6;
 
@@ -287,40 +256,24 @@ namespace Flame {
 		if (s_Data->QuadIndexCount >= Renderer2DData::MaxIndices)
 			FlushAndReset();
 
+		constexpr size_t quadVertexCount = 4;
 		const float textureIndex = 0.0f; // White Texture
+		constexpr glm::vec2 textureCoords[] = { { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 1.0f, 1.0f }, { 0.0f, 1.0f } };
 		const float tilingFactor = 1.0f;
 
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
 			* glm::rotate(glm::mat4(1.0f), glm::radians(rotation), { 0.0f, 0.0f, 1.0f })
 			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
 
-		s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[0];
-		s_Data->QuadVertexBufferPtr->Color = color;
-		s_Data->QuadVertexBufferPtr->TexCoord = { 0.0f, 0.0f };
-		s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
-		s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
-		s_Data->QuadVertexBufferPtr++;
-
-		s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[1];
-		s_Data->QuadVertexBufferPtr->Color = color;
-		s_Data->QuadVertexBufferPtr->TexCoord = { 1.0f, 0.0f };
-		s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
-		s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
-		s_Data->QuadVertexBufferPtr++;
-
-		s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[2];
-		s_Data->QuadVertexBufferPtr->Color = color;
-		s_Data->QuadVertexBufferPtr->TexCoord = { 1.0f, 1.0f };
-		s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
-		s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
-		s_Data->QuadVertexBufferPtr++;
-
-		s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[3];
-		s_Data->QuadVertexBufferPtr->Color = color;
-		s_Data->QuadVertexBufferPtr->TexCoord = { 0.0f, 1.0f };
-		s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
-		s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
-		s_Data->QuadVertexBufferPtr++;
+		for (size_t i = 0; i < quadVertexCount; i++)
+		{
+			s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[i];
+			s_Data->QuadVertexBufferPtr->Color = color;
+			s_Data->QuadVertexBufferPtr->TexCoord = textureCoords[i];
+			s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
+			s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
+			s_Data->QuadVertexBufferPtr++;
+		}
 
 		s_Data->QuadIndexCount += 6;
 
@@ -339,7 +292,9 @@ namespace Flame {
 		if (s_Data->QuadIndexCount >= Renderer2DData::MaxIndices)
 			FlushAndReset();
 
+		constexpr size_t quadVertexCount = 4;
 		constexpr glm::vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
+		constexpr glm::vec2 textureCoords[] = { { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 1.0f, 1.0f }, { 0.0f, 1.0f } };
 
 		float textureIndex = 0.0f;
 		for (uint32_t i = 1; i < s_Data->TextureSlotIndex; i++)
@@ -362,33 +317,15 @@ namespace Flame {
 			* glm::rotate(glm::mat4(1.0f), glm::radians(rotation), { 0.0f, 0.0f, 1.0f })
 			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
 		
-		s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[0];
-		s_Data->QuadVertexBufferPtr->Color = color;
-		s_Data->QuadVertexBufferPtr->TexCoord = { 0.0f, 0.0f };
-		s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
-		s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
-		s_Data->QuadVertexBufferPtr++;
-
-		s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[1];
-		s_Data->QuadVertexBufferPtr->Color = color;
-		s_Data->QuadVertexBufferPtr->TexCoord = { 1.0f, 0.0f };
-		s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
-		s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
-		s_Data->QuadVertexBufferPtr++;
-
-		s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[2];
-		s_Data->QuadVertexBufferPtr->Color = color;
-		s_Data->QuadVertexBufferPtr->TexCoord = { 1.0f, 1.0f };
-		s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
-		s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
-		s_Data->QuadVertexBufferPtr++;
-
-		s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[3];
-		s_Data->QuadVertexBufferPtr->Color = color;
-		s_Data->QuadVertexBufferPtr->TexCoord = { 0.0f, 1.0f };
-		s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
-		s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
-		s_Data->QuadVertexBufferPtr++;
+		for (size_t i = 0; i < quadVertexCount; i++)
+		{
+			s_Data->QuadVertexBufferPtr->Position = transform * s_Data->QuadVertexPositions[i];
+			s_Data->QuadVertexBufferPtr->Color = color;
+			s_Data->QuadVertexBufferPtr->TexCoord = textureCoords[i];
+			s_Data->QuadVertexBufferPtr->TexIndex = textureIndex;
+			s_Data->QuadVertexBufferPtr->TilingFactor = tilingFactor;
+			s_Data->QuadVertexBufferPtr++;
+		}
 
 		s_Data->QuadIndexCount += 6;
 
