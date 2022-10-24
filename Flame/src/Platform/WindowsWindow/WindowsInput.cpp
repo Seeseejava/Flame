f679233259fe3e95cf46d80ff108332474ea8a18
Seeseejava <49669869+Seeseejava@users.noreply.github.com>
2022-10-24 11:18:10 +0800

    Refactored Input "situation"
    --对于Input派生类来说，只有一个子类会被编译到最终的exe里，这是个Compile Time决定的事情，不需要借助虚函数来实现，因为一个Application只可能同时在一个Platform上运行。

--- Flame/src/Platform/WindowsWindow/WindowsInput.cpp
+++ Flame/src/Platform/WindowsWindow/WindowsInput.cpp
@@ -1,28 +1,30 @@
 #include "flamepch.h"
-#include "WindowsInput.h"
+#include "Flame/Core/Input.h"
 
 #include "Flame/Core/Application.h"
 #include <GLFW/glfw3.h>
 
+//对于Input派生类来说，只有一个子类会被编译到最终的exe里，这是个Compile Time决定的事情，不需要借助虚函数来实现，因为一个Application只可能同时在一个Platform上运行。
+//
+//所以更好的办法，是在Input类里创建一堆static函数，然后在各个平台对应的Input派生类类实现这些static函数
+
 namespace Flame {
 
-	Input* Input::s_Instance = new WindowsInput();//不是很理解为什么要这个
-
-	bool WindowsInput::IsKeyPressedImpl(int keycode)
+	bool Input::IsKeyPressed(int keycode)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetKey(window, keycode);
 		return state == GLFW_PRESS || state == GLFW_REPEAT;
 	}
 
-	bool WindowsInput::IsMouseButtonPressedImpl(int button)
+	bool Input::IsMouseButtonPressed(int button)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetMouseButton(window, button);
 		return state == GLFW_PRESS;
 	}
 
-	std::pair<float, float> WindowsInput::GetMousePositionImpl()
+	std::pair<float, float> Input::GetMousePosition()
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		double xpos, ypos;
@@ -31,16 +33,16 @@ namespace Flame {
 		return { (float)xpos, (float)ypos };
 	}
 
-	float WindowsInput::GetMouseXImpl()
+	float Input::GetMouseX()
 	{
-		auto [x, y] = GetMousePositionImpl();//C++17特性
+		auto [x, y] = GetMousePosition();//C++17特性
 		
 		return (float)x;
 	}
 
-	float WindowsInput::GetMouseYImpl()
+	float Input::GetMouseY()
 	{
-		auto v = GetMousePositionImpl();
+		auto v = GetMousePosition();
 
 		return std::get<0>(v);//C++14
 	}
