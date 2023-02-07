e013e4b99f91529301f1e23d341f17090b0666f0
Seeseejava <49669869+Seeseejava@users.noreply.github.com>
2023-02-07 15:29:24 +0800

    show Box3D

--- Engine/Source/Runtime/Renderer/Model.cpp
+++ Engine/Source/Runtime/Renderer/Model.cpp
@@ -1,5 +1,6 @@
 #include "flamepch.h"
 
+#include "Runtime/Resource/AssetManager/AssetManager.h"
 #include "Runtime/Renderer/Model.h"
 
 namespace Flame
@@ -13,7 +14,7 @@ namespace Flame
 	void Model::LoadModel(const std::string& path)
 	{
 		Assimp::Importer importer;
-		const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+		const aiScene* scene = importer.ReadFile(AssetManager::GetInstance().GetFullPath(path).string(), aiProcess_Triangulate | aiProcess_FlipUVs);
 
 		if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
 		{
